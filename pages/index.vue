e2a34d359751c0d9097a5d2fb974e6f934427261
Thomas <chearful@chearfulcraft.org>
2020-03-29 16:18:52 +0100

    Init nuxt project

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Mastermind
+      </h1>
+      <h2 class="subtitle">
+        The game, Mastermind
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
