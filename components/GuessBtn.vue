d1fffe392e230604ffebf678856a6ee2c560bad8
Thomas <chearful@chearfulcraft.org>
2020-03-29 18:13:57 +0100

    Guess input and display of current guess

---
+++ components/GuessBtn.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="guessbtn"
+    :style="{ 'background-color': colour }"
+    @click="addToGuess(colour)"
+  >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GuessBtn",
+  props: ["colour", "addToGuess"]
+};
+</script>
+
+<style>
+/* shamelessly stolen from https://www.w3schools.com/howto/howto_css_circles.asp because i dont do css */
+.guessbtn {
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  display: inline-block;
+  margin-left: 5%;
+}
+</style>
---
+++ pages/play.vue
@@ -0,0 +1,57 @@
+<template>
+  <b-container fluid>
+    <b-row>
+      <b-col sm="9">
+        <h1>Make your guess here</h1>
+        <p>Current input:</p>
+        <GuessBtn
+          v-for="colour in currentGuess"
+          :colour="colour"
+          :key="colour.name"
+          :addToGuess="addToGuess"
+        />
+        <p>Click on 4 dots in the order of your guess, and then click submit. Use the reset button if you wish to amend your guess before submitting.</p>
+        <GuessBtn
+          v-for="colour in colours"
+          :colour="colour.hex"
+          :key="colour.name"
+          :addToGuess="addToGuess"
+        />
+      </b-col>
+      <b-col>
+        <h1>Guesses</h1>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import GuessBtn from "../components/GuessBtn";
+export default {
+  components: {
+    GuessBtn
+  },
+  data: function() {
+    return {
+      colours: [
+        {name: "red", hex: "#e84118"},
+        {name: "green", hex: "#4cd137"},
+        {name: "blue", hex: "#00a8ff"},
+        {name: "dark blue", hex: "#273c75"},
+        {name: "yellow", hex: "#fbc531"},
+        {name: "purple", hex: "#9c88ff"}
+      ],
+      currentGuess: []
+    };
+  },
+  methods: {
+    addToGuess: function(colour) {
+      if (this.currentGuess.length == 4) {
+        alert("Only 4 pegs per guess, please...");
+        return;
+      }
+      this.currentGuess.push(colour);
+    }
+  }
+};
+</script>
