59fcd74c98acba25aac9a7d6a53a368d13e94e82
Thomas <chearful@chearfulcraft.org>
2020-03-29 17:04:47 +0100

    Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Mastermind</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+          <b-nav-item to="/rules">Guide/Rules</b-nav-item>
+          <b-nav-item to="/leaderboard">Leaderboard</b-nav-item>
+      </b-navbar-nav>
+
+      <b-navbar-nav class="ml-auto">
+        <b-button type="primary" to="/create-game">Play a game</b-button>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+};
+</script>
